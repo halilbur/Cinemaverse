d1a494037351cd68f201535c9d3fe6145a5efa73
halil <halil1414@yandex.com>
2023-12-23 23:11:38 +0300

    Redesign web page and api requests

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
-const apiKey = '7c40afd8aecb90ec1633b859bf146961';
-const apiUrlPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-const apiUrlGenres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
-const apiUrlUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`;
+//TMDB API USAGE AREA
 
-function displayMovies(movies, containerId) {
-  const movieContainer = document.getElementById(containerId);
+const API_KEY = 'api_key=7c40afd8aecb90ec1633b859bf146961';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-  movies.forEach(movie => {
-    const movieCard = document.createElement('div');
-    movieCard.className = 'movie-card';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+const genreURL = BASE_URL + '/genre/movie/list?' + API_KEY;
 
-    const movieImage = document.createElement('img');
-    movieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+const main= document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
-    const movieTitle = document.createElement('h3');
-    movieTitle.textContent = movie.title;
+getMovies(API_URL);
 
-    movieCard.appendChild(movieImage);
-    movieCard.appendChild(movieTitle);
-    movieContainer.appendChild(movieCard);
-  });
+function getMovies(url){
+  fetch(url).then(res => res.json()).then(data => {
+    showMovies(data.results);
+  })
 }
-function displayGenres(genres, containerId) {
-    const genreContainer = document.getElementById(containerId);
-  
-    genres.forEach(genre => {
-      const genreCard = document.createElement('div');
-      genreCard.className = 'genre-card';
-  
-      const genreName = document.createElement('h3');
-      genreName.textContent = genre.name;
-  
-      genreCard.appendChild(genreName);
-      genreContainer.appendChild(genreCard);
-    });
+
+function showMovies(data) {
+  main.innerHTML = '';
+
+  data.forEach(movie => {
+      const {title, poster_path, vote_average, overview, id} = movie;
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie');
+      movieEl.innerHTML = `
+           <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+
+          <div class="movie-info">
+              <h3>${title}</h3>
+              <span class="${getColor(vote_average)}">${vote_average}</span>
+          </div>
+
+          <div class="overview">
+
+              <h3>Overview</h3>
+              ${overview}
+              <br/> 
+              <button class="know-more" id="${id}">Know More</button
+          </div>
+      
+      `
+
+      main.appendChild(movieEl);
+
+      document.getElementById(id).addEventListener('click', () => {
+        console.log(id)
+        openNav(movie)
+      })
+  })
+}
+
+function getColor(vote){
+  if(vote>=8){
+    return 'green'
+  } else if(vote>= 5){
+    return 'orange'
+  } else {
+    return 'red'
   }
+}
 
-fetch(apiUrlPopular)
-  .then(response => response.json())
-  .then(data => {
-    displayMovies(data.results, 'featured');
-  })
-  .catch(error => console.error('Error fetching popular movies:', error));
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
 
-  fetch(apiUrlGenres)
-  .then(response => response.json())
-  .then(data => {
-    // For demonstration purposes, we'll display the first 10 genres as genre cards
-    displayGenres(data.genres.slice(0, 10), 'genres');
-  })
-  .catch(error => console.error('Error fetching genres:', error));
-
-fetch(apiUrlUpcoming)
-  .then(response => response.json())
-  .then(data => {
-    displayMovies(data.results, 'upcoming');
-  })
-  .catch(error => console.error('Error fetching upcoming movies:', error));
+  if(searchTerm){
+    getMovies(searchURL + '&query=' + searchTerm)
+  } else {
+    getMovies(API_URL);
+  }
+})
